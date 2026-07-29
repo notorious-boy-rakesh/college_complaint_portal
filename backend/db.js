e0270da241c5b5ea4ad23896c7e77917d04a9a56
Rakesh <rakesh.m212535@gmail.com>
2026-07-29 20:53:41 +0530

    deeeeeeee

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "rakesh",
   database: process.env.DB_NAME || "college_portal",
+  ssl: process.env.DB_HOST && process.env.DB_HOST !== "localhost" ? { rejectUnauthorized: true } : undefined,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
